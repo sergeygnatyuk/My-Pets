b2af0f2e87384cfe8333ef8f86d47c22238be68c
Sergey <Starcom12@mail.ru>
2021-05-06 02:03:43 +0300

    Starting new project

---
+++ The Chat/The Chat/Controllers/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  The Chat
+//
+//  Created by Гнатюк Сергей on 06.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        print("viewDidDisappear")
+    }
+}
+
